c9b3b3b305642a7f0e08519a713e04b9d955c130
hyeonhyejin <dev.hyejin04@gmail.com>
2020-11-17 15:37:26 +0900

    Init the project
      - install webpack
      - install babel
      - install eslint
      - install testing library : jest, testing-library, codeceptjs
      - install react

---
+++ codecept.conf.js
@@ -0,0 +1,35 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './test/*_test.js',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'http://localhost:8080',
+      show: false,
+      windowSize: '1200x900',
+    },
+  },
+  include: {
+    I: './steps_file.js',
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'final_project',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true,
+    },
+    tryTo: {
+      enabled: true,
+    },
+    screenshotOnFail: {
+      enabled: true,
+    },
+  },
+};
