7b48c1926098bbb7ecd95f5f9d535a247e4cd4db
hyeonhyejin <dev.hyejin04@gmail.com>
2020-11-20 15:59:01 +0900

    Implement a feature in App component according to e2e test
    After this commit, I will separate into components from the App commponent.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import {
+  questions,
+  options,
+} from './data';
+
 export default function App() {
   return (
-    <p>hello</p>
+    questions.map((question, i) => (
+      <div>
+        <p>{question}</p>
+        {options[i].map((option, _) => (
+          <div>
+            <label htmlFor={`option-${option}`}>{option}</label>
+            <input type="radio" name={`option-${i}`} id={`option-${option}`} value={option} />
+          </div>
+        ))}
+      </div>
+    ))
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,6 +1,4 @@
-Feature('option');
-
-const questions = [
+export const questions = [
   '1. 지역을 선택해 주세요.',
   '2. 나의 등산 실력은 어느 정도인가요?',
   '3. 어떤 난이도의 산을 원하나요?',
@@ -8,7 +6,7 @@ const questions = [
   '5. 산에서 무엇을 하고 싶으세요?',
 ];
 
-const options = [
+export const options = [
   [
     '서울',
     '부산', '인천', '대구', '광주', '대전', '울산',
@@ -20,22 +18,3 @@ const options = [
   ['봄', '여름', '가을', '겨울'],
   ['열심히 등산하기', '경치 구경하기', '금강산도 식후경', '술에 취하기'],
 ];
-
-Scenario('사용자는 각 단계에서 질문과 질문에 해당하는 옵션을 봅니다.', ({ I }) => {
-  I.amOnPage('/');
-  questions.forEach((question) => {
-    I.see(question);
-  });
-
-  options.flat().forEach((option) => {
-    I.see(option);
-  });
-});
-
-Scenario('사용자는 각 질문에서 옵션을 선택할 수 있습니다.', ({ I }) => {
-  I.amOnPage('/');
-
-  options.flat().forEach((option) => {
-    I.checkOption(option);
-  });
-});
