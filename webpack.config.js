15fdd3fd52467e2b55a732337050079ffe7cc6d8
hyejineee <44992033+hyejineee@users.noreply.github.com>
2020-12-07 19:07:16 +0900

    Revert "Create main home page "

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,6 @@ module.exports = {
       data: path.resolve(__dirname, 'src/data/'),
       service: path.resolve(__dirname, 'src/service/'),
       utils: path.resolve(__dirname, 'src/utils.js'),
-      page: path.resolve(__dirname, 'src/page/'),
     },
     extensions: ['.js', '.jsx'],
   },
